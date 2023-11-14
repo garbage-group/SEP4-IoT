81883ab23053193768780f642fc8560ee7157ddd
Domonkos Papp <325338@via.dk>
2023-11-14 12:22:15 +0100

    fiddling with readme newlines

--- test/test.c
+++ test/test.c
@@ -4,7 +4,7 @@ int main(int argc, char **argv)
 {
     UNITY_BEGIN();
     TEST_ASSERT_EQUAL_INT32(3, 3);
-    TEST_ASSERT_EQUAL_INT32(3, 4);
+    TEST_ASSERT_EQUAL_INT32(3, 3);
     UNITY_END();
 
     return 0;
