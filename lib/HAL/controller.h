c0a8e5624320864ddec63abf4aba71248f0ab5d1
Domonkos Papp <325338@via.dk>
2023-11-27 11:20:13 +0100

    tested temp and humid with hercules

--- lib/HAL/controller.h
+++ lib/HAL/controller.h
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
-void getHuimidty(uint8_t humidity_integer, uint8_t humidity_decimal, char *carray);
+void getHuimidty(uint8_t humidity_integer, uint8_t humidity_decimal);
 
-void getTemperature(uint8_t temperature_integer, uint8_t temperature_decimal, char *carray);
+void getTemperature(uint8_t temperature_integer, uint8_t temperature_decimal);
+
+void throwError();
--- src/main.c
+++ src/main.c
@@ -5,59 +5,58 @@
 #include "wifi.h"
 #include "controller.h"
 
-char carray[128]; 
+char carray[128];
 char rarray[128];
 uint8_t humidity_integer, humidity_decimal, temperature_integer, temperature_decimal;
 
-void receiveMessage(){
+void receiveMessage()
+{
     DHT11_ERROR_MESSAGE_t result = dht11_get(&humidity_integer, &humidity_decimal, &temperature_integer, &temperature_decimal);
 
-    if (result == DHT11_OK && strcmp(rarray, "getHumidity") == 0) {
-        getHuimidty(humidity_integer, humidity_decimal, carray);
-        //sprintf(carray, "it's currently %d,%d degrees celcius\n",temperature_integer, temperature_decimal);
-        //sprintf(carray, "humid:%d.%d:%s:%s:\n",humidity_integer,humidity_decimal);
-        //wifi_command_TCP_transmit(carray, 50);
-        //pc_comm_send_string_blocking(carray);
-    } 
-    if (result == DHT11_OK && strcmp(rarray, "getTemperature") == 0) {
-        getTemperature(temperature_integer, temperature_decimal, carray);
-    }
-    else 
+    if (result == DHT11_OK && strcmp(rarray, "getHumidity") == 0)
     {
-        sprintf(carray, "Failed to read data from DHT11 sensor\n");
-        wifi_command_TCP_transmit(carray, 40);
+        getHuimidty(humidity_integer, humidity_decimal);
+        }
+    if (result == DHT11_OK && strcmp(rarray, "getTemperature") == 0)
+    {
+        getTemperature(temperature_integer, temperature_decimal);
+    }
+    else
+    {
+        throwError();
     }
 }
 
-int main(){ 
+int main()
+{
     pc_comm_init(9600, NULL);
     pc_comm_send_string_blocking("start of program\n");
     dht11_init();
     wifi_init();
     _delay_ms(4000);
-    WIFI_ERROR_MESSAGE_t wifiresult = wifi_command_join_AP("Securi-ty","Sucction"); 
-    if(wifiresult == WIFI_OK)
+    WIFI_ERROR_MESSAGE_t wifiresult = wifi_command_join_AP("Securi-ty", "Sucction");
+    if (wifiresult == WIFI_OK)
     {
         pc_comm_send_string_blocking("connected\n");
-    }else
+    }
+    else
     {
         pc_comm_send_string_blocking("failed to connect\n");
     }
 
-    WIFI_ERROR_MESSAGE_t tcpResult = wifi_command_create_TCP_connection("192.168.230.197",2910,receiveMessage,rarray); 
+    WIFI_ERROR_MESSAGE_t tcpResult = wifi_command_create_TCP_connection("192.168.35.11", 23, receiveMessage, rarray);
     if (tcpResult == WIFI_OK)
     {
         pc_comm_send_string_blocking("TCP connected\n");
-    }else
+    }
+    else
     {
         pc_comm_send_string_blocking("TCP FAILED\n");
     }
-    
 
     while (1)
-    {        
-        //pc_comm_send_string_blocking("Hello from the arduino\n");
-        //sprintf(carray,"Hello from the arduino\n");
+    {
+        // pc_comm_send_string_blocking("Hello from the arduino\n");
+        // sprintf(carray,"Hello from the arduino\n");
     }
-    
 }
