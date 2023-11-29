4ebe523729a9e4459b78652e585f5e508cbeaf0d
Cudevlin <popcanindustries@gmail.com>
2023-11-29 14:03:59 +0100

    Completed getStatus Task

--- lib/HAL/status.c
+++ lib/HAL/status.c
@@ -2,9 +2,9 @@
 #include "dht11.h"
 #include <string.h>
 #include "hc_sr04.h"
-
-uint8_t humidity_integer, humidity_decimal, temperature_integer, temperature_decimal;
-uint16_t distance_result;
+#include "wifi.h"
+#include "pc_comm.h"
+#include <avr/delay.h>
 
 void sendOk(){
     char carray[128];
@@ -18,15 +18,4 @@ void sendNotOk(){
     sprintf(carray, "statu:NOT OK");
     wifi_command_TCP_transmit((uint8_t *)carray, strlen(carray));
     pc_comm_send_string_blocking(carray);
-}
-
-void getStatus(){
-    DHT11_ERROR_MESSAGE_t result = dht11_get(&humidity_integer, &humidity_decimal, &temperature_integer, &temperature_decimal);
-    distance_result = hc_sr04_takeMeasurement();
-
-    if(result == 1 && distance_result > -1 && distance_result < 4000){
-        sendOk();
-    } else{
-        sendNotOk();
-    }
 }
