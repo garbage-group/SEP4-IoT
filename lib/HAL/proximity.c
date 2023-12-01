4170a1482f90f810d6586942f16a8d863683aeb5
Domonkos Papp <325338@via.dk>
2023-12-01 14:06:28 +0100

    what the hell is wrong with the sensor

--- lib/HAL/proximity.c
+++ lib/HAL/proximity.c
@@ -6,11 +6,12 @@
 #include "proximity.h"
 #include <string.h>
 #include <stdlib.h>
+#include <avr/interrupt.h>
 
 uint16_t dtb;          // ditance to bottom
 double fillth = 100.0; // fill threshold when bin is considered full
 uint16_t aht = 0;      // the absolute height of the trash
-double newFull = 0;
+double relative = 0;
 double currentLevel = 0;
 
 int sendViaTCP(char *buffer)
@@ -23,7 +24,9 @@ void calibrateDevice()
     char carray[128];
     // sets dob to the current measurement from sensor
     //  to calibrate empty distance
+    // cli();
     dtb = hc_sr04_takeMeasurement();
+    // sei();
     sprintf(carray, "%d\n", dtb);
     sendViaTCP(carray);
     sprintf(carray, "Distance to bottom of bin is: %d mm\n", dtb);
@@ -34,12 +37,20 @@ void getCurrentLevel()
 { // Calculating fill level of bin
     char carray[128];
     uint16_t level = hc_sr04_takeMeasurement();
-    aht = dtb - level;
-    currentLevel = (aht / fillth) * 100;
-
-    dtostrf(currentLevel, 3, 1, carray);
-    sendViaTCP(carray);
+    dtostrf(level, 3, 1, carray);
     pc_comm_send_string_blocking(carray);
+    aht = dtb - level;
+    dtostrf(aht, 3, 1, carray);
+    pc_comm_send_string_blocking(carray);
+    currentLevel = (aht / dtb) * 100; // relative to full bin
+    dtostrf(currentLevel, 3, 1, carray);
+    pc_comm_send_string_blocking(carray);
+
+    relative = (currentLevel / fillth) * 100;
+
+    dtostrf(relative, 3, 1, carray);
+    pc_comm_send_string_blocking(carray);
+    sendViaTCP(carray);
 }
 
 void setThreshold(double threshold)
@@ -48,8 +59,7 @@ void setThreshold(double threshold)
     // sets global variable to given threshold
     fillth = threshold; // because threhsold format is precentage double ex: 70.0. This is explicitly defined in the interfacing contract :)
 
-    dtostrf(threshold, 3, 1, carray);
-    // sprintf(carray, "%Lf\n", threshold);
+    dtostrf(threshold, 3, 3, carray);
     pc_comm_send_string_blocking(carray);
     sendViaTCP("setft:OK\n");
 }
--- src/main.c
+++ src/main.c
@@ -1,7 +1,7 @@
 #include "pc_comm.h"
 #include <avr/delay.h>
 #include "dht11.h"
-#include "hc_sr04.h"
+// #include "hc_sr04.h"
 #include <stdio.h>
 #include "wifi.h"
 #include "dht_controller.h"
@@ -11,10 +11,39 @@
 #include "activate_buzzer.h"
 #include "string.h"
 #include "status.h"
+#include <stdlib.h>
 
 char rarray[128];
 uint8_t humidity_integer, humidity_decimal, temperature_integer, temperature_decimal;
 uint16_t level_integer;
+
+double parse_double(const char *string)
+{
+    char carray[128];
+    char string_buffer[128];
+    int decimal, precision;
+    char *ptr;
+    int power;
+    double dividor = 1;
+    sprintf(string_buffer, "%s", string);
+    // 70.001
+
+    char *point_position = strchr(string_buffer, '.'); // saves the pointer in openParen to the char before the value "("
+
+    ptr = point_position + 1;
+    power = strlen(ptr);
+    sscanf(ptr, "%d", &precision);
+
+    *point_position = '\0';
+    sscanf(string_buffer, "%d", &decimal);
+
+    for (int i = 1; i <= power; i++)
+        dividor *= 10;
+
+    sprintf(carray, "DEBUG: %d,%d,%d,%s,%s\n", decimal, precision, power, ptr, string);
+    pc_comm_send_string_blocking(carray);
+    return (double)decimal + ((double)precision / dividor);
+};
 
 void receiveMessage()
 {
@@ -37,10 +66,27 @@ void receiveMessage()
     {
         calibrateDevice();
     }
-    if (strcmp(rarray, "setFillThreshold") == 0)
+    if (strncmp(rarray, "setFill", 7) == 0)
+
     {
-        setThreshold(70.0); // the number should be sent by
+        char carray[200];
+        const char *openParen = strchr(rarray, '('); // saves the pointer in openParen to the char before the value "("
+        char *closedParen = strchr(rarray, ')');
+        if (closedParen != NULL)
+            *closedParen = '\0';
+
+        pc_comm_send_string_blocking(openParen);
+
+        pc_comm_send_string_blocking("\n");
+        if (openParen != NULL)
+        {
+            double value;
+            value = parse_double(openParen + 1);
+
+            setThreshold(value); // The number is sent by cloud this way
+        }
     }
+
     if (strcmp(rarray, "getCurrentLevel") == 0)
     {
         getCurrentLevel(level_integer);
@@ -51,17 +97,20 @@ void receiveMessage()
     }
     if (strcmp(rarray, "getStatus") == 0)
     {
-        if(result == DHT11_OK && (int)level_integer > 49 && (int) level_integer < 3001){
-        sendOk();
-        } else {
-        sendNotOk();
+        if (result == DHT11_OK && (int)level_integer > 49 && (int)level_integer < 3001)
+        {
+            sendOk();
+        }
+        else
+        {
+            sendNotOk();
         }
     }
 }
 
 int create_TCP_connection()
 {
-    WIFI_ERROR_MESSAGE_t tcpResult = wifi_command_create_TCP_connection("172.20.10.14", 5663, receiveMessage, rarray);
+    WIFI_ERROR_MESSAGE_t tcpResult = wifi_command_create_TCP_connection("192.168.0.68", 5663, receiveMessage, rarray);
     if (tcpResult == WIFI_OK)
     {
         pc_comm_send_string_blocking("TCP connected\n");
@@ -82,7 +131,7 @@ int main()
     hc_sr04_init();
     wifi_init();
     _delay_ms(4000);
-    WIFI_ERROR_MESSAGE_t wifiresult = wifi_command_join_AP("bigdaddy", "22222223");
+    WIFI_ERROR_MESSAGE_t wifiresult = wifi_command_join_AP("FTTH_MP8523", "syemRekWeed4");
     if (wifiresult == WIFI_OK)
     {
         pc_comm_send_string_blocking("connected\n");
@@ -95,10 +144,12 @@ int main()
 
     while (1)
     {
-        //_delay_ms(5000);
+
         // wifi_command_close_TCP_connection();
         // create_TCP_connection();
         // pc_comm_send_string_blocking("Hello from the arduino\n");
         // sprintf(carray,"Hello from the arduino\n");
     }
+
+
 }
